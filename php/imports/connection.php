fe01e9ce40648f906c1fef7dfc9725fc3676b27e
Saul Maylin <96966989+MrJesterBear@users.noreply.github.com>
2025-06-27 18:05:05 +0100

    Added DB Con
    - Added connection.php which connects to the database using CFG held on the webserver.
    
    - Adds some basic error handling with console logging.

---
+++ php/imports/connection.php
@@ -0,0 +1,41 @@
+<!-- ? Name:  Saul Maylin
+? Date: 27/06/2025
+? v1
+? Project: Project Crawler
+? Connection to the database
+-->
+
+<?php 
+function loadDatabaseConfig($vars = []) {
+    // get config from php.ini CFG
+    foreach ($vars as $v) {
+        define($v, get_cfg_var("myapp.cfg.$v"));
+    }
+}
+
+function connectToDatabase() {
+    // Prepare database connection with neded parameters
+    $host = ['DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME'];
+
+    // Send params to be parsed
+    loadDatabaseConfig($host);
+
+    try {
+        // open connection to the database
+        @$DB = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        return true;
+    } catch (Exception $e) {
+        // Handle connection error
+        echo "Connection failed: " . $e->getMessage();
+        return false;
+
+    }
+}
+
+// Run previous function to connect to the database
+if (connectToDatabase()) {
+    echo "<script> console.log('Database connection successful!'); </script>";
+} else {
+    echo "<script> console.error('Failed to connect to the database.'); </script>";
+}
+?>
---
+++ php/server/Account.php
@@ -0,0 +1,14 @@
+<!-- ? Name:  Saul Maylin
+? Date: 27/06/2025
+? v1
+? Project: Project Crawler
+? Check or Register an Account
+-->
+    <?php 
+
+    include ('../imports/error.php');
+
+    // Include the connection file
+    include ('../imports/connection.php');
+
+    ?>
