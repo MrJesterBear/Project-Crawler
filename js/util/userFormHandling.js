23e9bac3387bd08189a60d9d075dbb42c6994f47
Saul Maylin <96966989+MrJesterBear@users.noreply.github.com>
2025-06-29 01:34:59 +0100

    Update userFormHandling.js
    - Moved error switch inside both AJAX as cannot reference a method in the same class. could maybe have made it static, but i digress. just want this to work, so a note for next time.

--- js/util/userFormHandling.js
+++ js/util/userFormHandling.js
@@ -27,7 +27,29 @@ class userFormHandling {
       success: function (data) {
         // When the php file has been executed succesfully.
         console.log("register response:", data);
-        errorHandler(data.error); // checks if there is an error in the response.
+        switch (data.error) {
+          case "NOT_FOUND": // User not found.
+            window.location.href = "./new-user.php?error=NF";
+            break;
+          case "DUP": // email or username already exists.
+            window.location.href = "./new-user.php?error=DUP";
+            break;
+          case "PASS": // Password incorrect.
+            window.location.href = "./new-user.php?error=PASS";
+            break;
+          case "REG": // Registration failed, server down?
+            window.location.href = "./new-user.php?error=REG";
+            break;
+          case "UID": // Failed UID check, server down?
+            window.location.href = "./new-user.php?error=UID";
+            break;
+          case "NULL": // No error, user logged in successfully
+            window.location.href = "./account.php";
+            break;
+          default: // Unknown error
+            // window.location.href = "./new-user.php?error=UNKNOWN";
+            break;
+        }
       },
       error: function (xhr, status, error) {
         console.error("Error registering in user:", error, status);
@@ -46,38 +68,34 @@ class userFormHandling {
       },
       success: function (data) {
         console.log("Login response:", data);
-        errorHandler(data.error);
+        switch (data.error) {
+          case "NOT_FOUND": // User not found.
+            window.location.href = "./new-user.php?error=NF";
+            break;
+          case "DUP": // email or username already exists.
+            window.location.href = "./new-user.php?error=DUP";
+            break;
+          case "PASS": // Password incorrect.
+            window.location.href = "./new-user.php?error=PASS";
+            break;
+          case "REG": // Registration failed, server down?
+            window.location.href = "./new-user.php?error=REG";
+            break;
+          case "UID": // Failed UID check, server down?
+            window.location.href = "./new-user.php?error=UID";
+            break;
+          case "NULL": // No error, user logged in successfully
+            window.location.href = "./account.php";
+            break;
+          default: // Unknown error
+            // window.location.href = "./new-user.php?error=UNKNOWN";
+            break;
+        }
       },
       error: function (xhr, status, error) {
         console.error("Error logging in user:", error, status);
         window.location.href = "./new-user.php?error=UNKNOWN";
       },
     });
-  }
-
-  errorHandler(error) {
-    switch (error) {
-      case "NOT_FOUND": // User not found.
-        window.location.href = "./new-user.php?error=NF";
-        break;
-      case "DUP": // email or username already exists.
-        window.location.href = "./new-user.php?error=DUP";
-        break;
-      case "PASS": // Password incorrect.
-        window.location.href = "./new-user.php?error=PASS";
-        break;
-      case "REG": // Registration failed, server down?
-        window.location.href = "./new-user.php?error=REG";
-        break;
-      case "UID": // Failed UID check, server down?
-        window.location.href = "./new-user.php?error=UID";
-        break;
-      case "NULL": // No error, user logged in successfully
-        window.location.href = "./account.php";
-        break;
-      default: // Unknown error
-        // window.location.href = "./new-user.php?error=UNKNOWN";
-        break;
-    }
   }
 }
