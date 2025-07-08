245406d1d267dc443beb8170cf5b6ea550b23819
Saul Maylin <96966989+MrJesterBear@users.noreply.github.com>
2025-07-08 23:34:29 +0100

    Create forgot-password.php

---
+++ forgot-password.php
@@ -0,0 +1,73 @@
+<!-- ? Name:  Saul Maylin
+? Date: 08/07/2025
+? v1
+? Project: Project Crawler
+? -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Forgotten Password | Project Crawler</title>
+
+    <!-- * Meta data for indexing-->
+    <meta name="description" content="The home page for a Dungeon Master vault and D&D Beyond clone." />
+    <meta name="keywords" content="D&D, Character Creator, tabletop, rpg" />
+    <meta name="author" content="Saul Maylin" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="index, follow" />
+
+    <!-- * Links for both the stylesheet for pazas and a favicon for the site.-->
+    <!-- ! Import Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous" />
+    <link rel="icon" type="image/x-icon"
+        href="/assets/universal/4048494a-3811-4b3f-a59a-cb6ef91501dd_1.56f67623514375c32ce7bbf383a5974a.jpeg" />
+</head>
+
+<body class="bodyDefault">
+
+    <!-- * Image & Nav -->
+    <div class="d-flex justify-content-center">
+        <a class="navbar-brand" href="index.html">
+            <img src="https://images.emojiterra.com/twitter/v13.1/512px/1f3b2.png" alt="Dice" width="90">
+        </a>
+    </div>
+
+    <nav class="nav navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-center">
+        <script type="module">
+            // imports setnav function and runs it.
+            import { setNav } from "./js/html/nav.js";
+            setNav();
+        </script>
+    </nav>
+
+    <!-- * Main Content -->
+
+    <div class="container-fluid text-center">
+        <h1 class="display-1">Forgotten Password</h1>
+        <p class="lead">If you have forgotten your password, enter your email to send a reset link.</p>
+        <!-- <p class="lead">This is a work in progress </p> -->
+    </div>
+
+    <div class="d-flex justify-content-center text-center my-3">
+        <div class="row">
+            <!-- Login -->
+            <div class="border col-md mx-5">
+
+            </div>
+        </div>
+
+
+        <!-- Footer -->
+        <div class="container-fluid text-center bg-secondary border border-border">
+            <p class="text-white">This is a footer! yarharr.</p>
+        </div>
+
+        <!-- ! Import bootstrap JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
